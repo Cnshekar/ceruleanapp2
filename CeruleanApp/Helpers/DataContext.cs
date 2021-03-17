8515a07f669d8efd82be6cb01c471082d9ad14c8
Cnshekar <cnshekar123@gmail.com>
2021-03-18 00:35:59 +0530

    initial commit

---
+++ CeruleanApp/Helpers/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using CeruleanApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeruleanApp.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
